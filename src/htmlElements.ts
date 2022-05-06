1fb894fc5b070a5d5f8d971ef73676a5611b380d
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-05-05 23:45:16 -0300

    Clase N°10: POST: guardando michis favoritos.

--- src/htmlElements.ts
+++ src/htmlElements.ts
@@ -5,3 +5,7 @@ export const button = document.getElementById('main__button-refresh-id') as HTML
 export const errorSection = document.getElementById('main__error-section-id') as HTMLElement;
 export const imgHttpCode = document.getElementById('error-section__http-code-errors-img-id') as HTMLImageElement;
 export const pError = document.getElementById('main__error-id') as HTMLParagraphElement;
+export const buttonFavorite1 = document.getElementById('ramdon-michis__button-1-id') as HTMLButtonElement;
+export const buttonFavorite2 = document.getElementById('ramdon-michis__button-2-id') as HTMLButtonElement;
+
+export const imgs = [img1, img2];
