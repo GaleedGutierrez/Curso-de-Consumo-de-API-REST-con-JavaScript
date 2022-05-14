3867eb142f8b5a23e81b212e86274ae80512ec21
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-05-14 15:27:17 -0300

    Carpeta dist agregada.

---
+++ dist/eventsListeners.js
@@ -0,0 +1,4 @@
+import { API_URL_RANDOM } from "./api.js";
+import { buttonRefresh, imgs } from "./htmlElements.js";
+import { loadRandomMichis } from "./main.js";
+buttonRefresh.onclick = () => loadRandomMichis(imgs, API_URL_RANDOM);
